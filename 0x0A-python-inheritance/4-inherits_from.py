feafb71a66c855237980b221479f968c7fc9fd56
ogemboeugene <7103.2021@students.ku.ac.ke>
2024-01-08 12:01:33 -0800

    python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
-
 def inherits_from(obj, a_class):
-    """Determines if the object is an instance of a class or inherited
+    """
+    Determines if the object is an instance of a class or inherited (directly or indirectly)
     Args:
         obj (object): object
         a_class (class): class to compare to
     Returns:
         True if object is instance or inherited from class, False otherwise
     """
-    return type(obj) is not a_class and issubclass(type(obj), a_class)
+    obj_class = type(obj)
+    
+    # Check if the object's class is not the specified class
+    if obj_class is not a_class:
+        # Check if the specified class is in the object's class MRO (Method Resolution Order)
+        for inherited_class in obj_class.__mro__:
+            if inherited_class is a_class:
+                return True
+    
+    return False
