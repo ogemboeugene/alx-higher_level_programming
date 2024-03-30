cd327659a90ca061abdc13bce2f6c46589da4295
ogemboeugene <7103.2021@students.ku.ac.ke>
2024-03-30 02:17:00 -0700

    change

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,8 +8,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
-    r = requests.get(url).json()
-    for i in range(10):
-        print("{}: {}".format(
-            r[i].get('sha'), r[i].get('commit').get('author').get('name')))
+        url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+        r = requests.get(url).json()
+        for i in range(10):
+            print("{}: {}".format(
+        r[i].get('sha'), r[i].get('commit').get('author').get('name')))
